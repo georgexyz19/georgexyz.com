e163d2651ccfc5b23bf47c1ddcba864e32c830f2
George Zhang <guanghua19@gmail.com>
2020-11-10 23:54:39 -0500

    venv version update

---
+++ commit_change_venv.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# commit_change.sh : commit git changes for georgexyz.com 11/10/2020
+# This is venv version of the script
+# How to execute the script:
+# $source venv/bin/activate
+# $source commit_change_venv.sh "git commit message"
+# I will start to use venv on new computers with this script
+
+PROGNAME="$(basename "$0")" # or = "${0##*/}"
+
+usage () {
+  cat << EOF
+Usage: $PROGNAME "git commit message"
+Where:
+  git commit message: is a message passed to 'git commit -m' 
+EOF
+}
+
+if [ -z "$1" ]; then 
+    usage
+    exit 1
+fi
+
+git pull
+
+# pipenv shell # does not work
+# publish is clean and build with publishconf.py
+invoke publish 
+git status
+git add -A
+git commit -m "$1"
+git push origin master # update source code
+
+ghp-import output -b gh-pages
+git push origin gh-pages
